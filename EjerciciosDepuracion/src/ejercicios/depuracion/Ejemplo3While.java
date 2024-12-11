665cce735d8455c6ab7f96243ff0a2e4c2b997ff
rdavidja02 <rdavidja02@educastur.es>
2024-12-11 01:21:01 +0100

    PASOSEBRA Y =15

--- EjerciciosDepuracion/src/ejercicios/depuracion/Ejemplo3While.java
+++ EjerciciosDepuracion/src/ejercicios/depuracion/Ejemplo3While.java
@@ -8,7 +8,7 @@ public class Ejemplo3While {
     public static void main(String[] args) {
         final int VALOR_INICIAL = 10;  // limite inf. tabla
         final int VALOR_FINAL = 100;  // limite sup. tabla
-        final int PASO = 10 ; // incremento
+        final int PASOSEBRA = 15 ; // incremento
         double fahrenheit;
         double celsius;
         fahrenheit = VALOR_INICIAL;
@@ -16,7 +16,7 @@ public class Ejemplo3While {
         while (fahrenheit <= VALOR_FINAL ){
                celsius = 5*(fahrenheit - 32)/9.0;
                System.out.printf("%7d \t %8.3f \n", fahrenheit, celsius);
-               fahrenheit += PASO;
+               fahrenheit += PASOSEBRA;
         }
     }
 }
